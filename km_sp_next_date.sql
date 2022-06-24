16d0f1deb42e1dc1b16ec3d2d41d931313cd7d42
kmbeyond <kiran.learn7@gmail.com>
2022-06-24 10:21:44 -0400

    Create km_sp_next_date.sql

---
+++ km_sp_next_date.sql
@@ -0,0 +1,34 @@
+//USE ROLE xx;
+//USE WAREHOUSE xx;
+//USE DATABASE xx;
+//USE SCHEMA xx;
+
+create or replace procedure km_sp_next_date(STR_DATE VARCHAR )
+    RETURNS STRING
+    LANGUAGE JAVASCRIPT
+    COMMENT = "KM Test Stored Proc to return next day"
+    EXECUTE AS CALLER
+AS
+$$
+ function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+ }
+
+ let date_next =  new Date(STR_DATE+"T12:00:00Z");
+ date_next.setDate(date_next.getDate() + 1);
+ return formatDate(date_next);
+$$
+
+
+//call km_sp_next_date('2022-01-04');
+//=> 2022-01-05
