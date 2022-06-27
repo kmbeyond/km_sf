9eeb7e6e96c39e0f9342d015b886de84657074e1
kmbeyond <kiran.learn7@gmail.com>
2022-06-27 12:17:44 -0400

    Update km_sp_format_date.sql

--- km_sp_format_date.sql
+++ km_sp_format_date.sql
@@ -1,12 +1,21 @@
+/*
+SP to return String date in MM-DD-YYYY
+Input:
+ 1.Date string
+ 2.Format of date string
+ 3.delimiter
+
+*/
+
 //USE ROLE xx;
 //USE WAREHOUSE xx;
 //USE DATABASE xx;
 //USE SCHEMA xx;
 
-create or replace procedure km_sp_format_date(STR_DATE VARCHAR )
+create or replace procedure km_sp_format_date(STR_DATE VARCHAR, DT_FORMAT VARCHAR, DELIMITER VARCHAR )
     RETURNS STRING
     LANGUAGE JAVASCRIPT
-    COMMENT = "KM Test Stored Proc to return date"
+    COMMENT = "KM Test Stored Proc to return date in MM-DD-YYYY"
     EXECUTE AS CALLER
 AS
 $$
@@ -22,10 +31,26 @@ $$
 	var month = parseInt(dateItems[monthIndex]);
 	return new Date(dateItems[yearIndex], month-1, dateItems[dayIndex]);
  }
- return stringToDate(STR_DATE, "yyyy-mm-dd", "-");
+ function prefixZero(i) {
+  if (i < 10) {i = "0" + i}
+  return i;
+ }
+ function formatDate(date) {
+    var d = new Date(date),
+        month = prefixZero(d.getMonth() + 1),
+        day = prefixZero(d.getDate()),
+        year = d.getFullYear();
+
+    return [month, day, year].join('-');
+ }
+ return "Date (MM-DD-YYYY): " + formatDate(stringToDate(STR_DATE, DT_FORMAT, DELIMITER));
 $$
 
 
+call km_sp_format_date('2022-04-01', 'yyyy-mm-dd', '-');
+-> Date (MM-DD-YYYY): 04-01-2022
 
-call km_sp_format_date('2022-02-04');
-=> Fri Feb 04 2022 00:00:00 GMT-0800 (Pacific Standard Time)
+call km_sp_format_date('05/02/2022', 'mm/dd/yyyy', '/');
+-> Date (MM-DD-YYYY): 05-02-2022
+
+
