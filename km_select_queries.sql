4292262f1a175aaf779849bf60cc467296ac13d1
kmbeyond <kiran.learn7@gmail.com>
2023-10-25 17:20:55 -0400

    string_to_datetime_to_string

--- km_select_queries.sql
+++ km_select_queries.sql
@@ -1,5 +1,13 @@
 
 
+
+---using datetimes: string->datetime->string
+WITH file_data as (SELECT '20231025104914_debit.txt' as file_name)
+SELECT concat(to_char(TO_TIMESTAMP(substring(file_name,0,8), 'YYYYMMDD'), 'YYYY-MM-DD'), ' ', to_char(TO_TIMESTAMP(substring(file_name,9,6), 'HH24MISS'), 'HH24:MI:SS')) as file_time
+ FROM file_data;
+--> 2023-10-25 10:49:14
+
+
 ---using regexp in filter & regexp_substr()
 set audit_dt_p = '20231024';
 
