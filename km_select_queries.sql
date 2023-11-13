0d5a408063b2f43287ef0b8b18d44a5b72dc3200
kmbeyond <kiran.learn7@gmail.com>
2023-11-13 17:09:58 -0500

    date/time values

--- km_select_queries.sql
+++ km_select_queries.sql
@@ -1,11 +1,30 @@
 
 
 
+current_date() as record_create_date,
+to_varchar(CURRENT_DATE(), 'yyyy-mm-dd') as TRANSACTION_DT
+
+CURRENT_TIMESTAMP() as RECORD_CREATE_TIME,
+CURRENT_USER() as RECORD_CREATED_BY_USER,
+CURRENT_ROLE() as RECORD_CREATED_BY_ROLE,
+
+
+---add/subtract days to/from current date/time
+to_varchar(dateadd(day, 2, CURRENT_DATE()), 'yyyy-mm-dd') as S_TRANSACTION_DATE,
+--subtract random days from current date/time
+to_varchar(dateadd(day, (uniform(0, 5, random())*-1),CURRENT_DATE()), 'yyyy-mm-dd') as S_TRANSACTION_DATE,
+dateadd(day,(uniform(0, 4, random())*-1),current_timestamp()) as TRANSACTION_DTTM,
+
+
 ---using datetimes: string->datetime->string
 WITH file_data as (SELECT '20231025104914_debit.txt' as file_name)
 SELECT concat(to_char(TO_TIMESTAMP(substring(file_name,0,8), 'YYYYMMDD'), 'YYYY-MM-DD'), ' ', to_char(TO_TIMESTAMP(substring(file_name,9,6), 'HH24MISS'), 'HH24:MI:SS')) as file_time
  FROM file_data;
 --> 2023-10-25 10:49:14
+
+
+-----append random number to a string to make it unique
+concat(TRANSACTION_ID, uniform(0, 1000, random())) as TRANSACTION_ID,
 
 
 ---using regexp in filter & regexp_substr()
