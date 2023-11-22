aa01443a882c771d5722fd5bf0bb7659ea49b278
kmbeyond <kiran.learn7@gmail.com>
2023-11-22 16:22:09 -0500

    query timestamp column

--- km_select_queries.sql
+++ km_select_queries.sql
@@ -29,6 +29,7 @@ SELECT concat(to_char(TO_TIMESTAMP(substring(file_name,0,8), 'YYYYMMDD'), 'YYYY-
 --CAUTION with queries on datetime/timestamp columns: TIMESTAMP_NTZ
 AND record_create_time between concat(current_date()-1, ' 00:00:00') AND concat(current_date()-1, ' 23:59:59.999') --
 AND record_create_time BETWEEN concat(current_date()-1, ' 00:00:00') AND current_date()
+AND record_create_time BETWEEN current_date()-1 AND current_date()
 
 --AND to_char(record_create_time, '%Y-%m-%d')=current_date()-1  --'2023-11-15'  --EXPENSIVE
 --AND substr(record_create_time,0,10)=current_date()-1 --'2023-11-15'    --EXPENSIVE
